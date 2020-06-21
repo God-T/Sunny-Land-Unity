eb6160fa5a8b6064396e6a30b72c74630e197bc8
Shuyu <s.zong@student.unsw.edu.au>
2020-06-22 01:12:25 +1000

    Add player animation & apply to player movements

--- Assets/Script/playerControl.cs
+++ Assets/Script/playerControl.cs
@@ -4,26 +4,69 @@ using UnityEngine;
 
 public class playerControl : MonoBehaviour
 {
-
     public Rigidbody2D rb;
+    public Animator anim;
     public float speed;
+    public float jumpForce;
 
     // Start is called before the first frame update
-    void Start() {
-    
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate()
+    {
         Movement();
+        SwitchAnimation();
     }
 
-    void Movement() {
-        float moveH= Input.GetAxis("Horizontal");//L:-1; R:1
-        Debug.Log("Hello: " + moveH);
-        if (moveH != 0) {
-            rb.velocity = new Vector2(moveH * speed, rb.velocity.y);
+    void Movement()
+    {
+        float moveH = Input.GetAxis("Horizontal");//L:-1; R:1
+        float faceDir = Input.GetAxisRaw("Horizontal");
+        // Debug.Log(moveH);
+
+        //player horizontal movements
+        if (moveH != 0)
+        {
+            rb.velocity = new Vector2(moveH * speed * Time.deltaTime, rb.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(faceDir));
+        }
+        //player facing
+        if (faceDir != 0)
+        {
+            transform.localScale = new Vector3(faceDir, 1, 1);
+        }
+        //player jump
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log(jumpForce * Time.deltaTime);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
+            anim.SetBool("jumping", true);
+        }
+
+    }
+
+
+    void SwitchAnimation()
+    {
+        if (anim.GetBool("jumping"))
+        {
+            if (rb.velocity.y < 0)
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("falling", true);
+            }
+        }
+        else if (anim.GetBool("falling"))
+        {
+            if (rb.velocity.y == 0)
+            {
+                anim.SetBool("falling", false);
+                anim.SetBool("idle", true);
+            }
         }
     }
-
 }
