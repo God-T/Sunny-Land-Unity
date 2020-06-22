ac7d1177a523e4eadc14ddfab8cc260ebdb6a5f0
Shuyu <s.zong@student.unsw.edu.au>
2020-06-23 00:20:40 +1000

    Optimize code with Update() and FixedUpdate()

--- Assets/Script/playerControl.cs
+++ Assets/Script/playerControl.cs
@@ -10,70 +10,82 @@ public class playerControl : MonoBehaviour
     public Collider2D coll;     //get Collider2d
     public LayerMask ground;    //get ground layer
 
-    public float speed;         //init horizontal movement speed
-    public float jumpForce;     //init jumping speed
+    [Range(0, 1000)] [SerializeField] public float speed = 0f;         //init horizontal movement speed
+    [Range(0, 1000)] [SerializeField] public float jumpForce = 0f;     //init jumping speed
 
+    float moveH, faceDir = 0f;
+    bool isJump = false;
 
-    /*Start is called before the first frame update*/
-    void Start()
-    {
-
-    }
 
     /*Update is called once per frame*/
-    void FixedUpdate()
+
+    void Update()
     {
-        Movement();
-        SwitchAnimation();
+        moveH = Input.GetAxis("Horizontal");//L:-1; R:1
+        faceDir = Input.GetAxisRaw("Horizontal");
+        AnimationRendering();
     }
 
-    void Movement()
-    {
-        float moveH = Input.GetAxis("Horizontal");//L:-1; R:1
-        float faceDir = Input.GetAxisRaw("Horizontal");
-        // Debug.Log(moveH);
 
-        /*player horizontal movements*/
-        if (moveH != 0)
+
+    void FixedUpdate()
+    {
+        OnMovement();
+        if (isJump)
         {
-            rb.velocity = new Vector2(moveH * speed * Time.deltaTime, rb.velocity.y);
-            anim.SetFloat("running", Mathf.Abs(faceDir));
+            Debug.Log("fixed " + isJump);
+            OnJumping();
         }
-        /*player facing*/
+        OnFacing();
+    }
+
+    /*handle player horizontal movements*/
+    void OnMovement()
+    {
+        rb.velocity = new Vector2(moveH * speed * Time.deltaTime, rb.velocity.y);
+    }
+
+    /*handle player jumping*/
+    void OnJumping()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
+        isJump = false;
+    }
+
+    /*handle player facing direction*/
+    void OnFacing()
+    {
         if (faceDir != 0)
         {
             transform.localScale = new Vector3(faceDir, 1, 1);
         }
-        /*player jump*/
-        if (Input.GetButton("Jump"))
-        {
-            Debug.Log(jumpForce * Time.deltaTime);
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
-            /*change state to jumping*/
-            anim.SetBool("jumping", true);
-        }
-
     }
 
-
-    void SwitchAnimation()
+    /*handle player animation states*/
+    void AnimationRendering()
     {
-
-        anim.SetBool("idle", false);
-        /*jumping state => falling state*/
-        if (anim.GetBool("jumping"))
+        /*running or idle*/
+        anim.SetFloat("RunningSpeed", Mathf.Abs(faceDir));
+        /*start jumping*/
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
         {
-            if (rb.velocity.y < 0)
-            {
-                anim.SetBool("jumping", false);
-                anim.SetBool("falling", true);
-            }
+            anim.SetBool("IsJumping", true);
+            isJump = true;
         }
-        /*falling state => idle*/
-        else if (coll.IsTouchingLayers(ground))
+
+        /*start falling*/
+        if (anim.GetBool("IsJumping") && rb.velocity.y < 0)
         {
-            anim.SetBool("falling", false);
-            anim.SetBool("idle", true);
+            Debug.Log("anim1 " + isJump);
+
+            anim.SetBool("IsJumping", false);
+            anim.SetBool("IsFalling", true);
+            isJump = false;
+        }
+        /*back on ground*/
+        else if (anim.GetBool("IsFalling") && coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("IsFalling", false);
         }
     }
 }
